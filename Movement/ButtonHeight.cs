e9d88abf876ba59e4a3e2fa09d0a82b97cc96155
seancunnington <cunnington.sean@gmail.com>
2020-02-15 20:03:09 -0800

    Salmorine code upload.

--- Movement/ButtonHeight.cs
+++ Movement/ButtonHeight.cs
@@ -8,22 +8,10 @@
         public float buttonAdd;
         private GameObject shipHullRef;
 
-        
-
         public override void StartTouching(VRTK_InteractTouch usingObject)
         {
             base.StartTouching(usingObject);
-            /*
-            //JUMP
-            //if at 3 max AND buttonAdd == 1 AND touching RiverCollision, then jump
-            if (shipHullRef.GetComponent<MovePiece>().step_count_up == 3 && buttonAdd == 1 && shipHullRef.GetComponent<MovePiece>().jumpCheck == true)
-            {
-                shipHullRef.GetComponent<MovePiece>().jumpBool = true;
-                shipHullRef.GetComponent<MovePiece>().jumpMotion = 20f;
-                shipHullRef.GetComponent<MovePiece>().selfGravity = 0.5f;
-            }
-            */
-            //MOVE
+
             //if at 3 max and buttonAdd == -1, then okay to add
             if (shipHullRef.GetComponent<MovePiece>().step_count_up == 3 && buttonAdd == -1)
             {
--- Movement/MovePiece.cs
+++ Movement/MovePiece.cs
@@ -12,18 +12,6 @@
         private GameObject playArea;
         private float circleNum;
 
-        //jump check prior to the jump bool -- indicates collision with RiverCollision
-        //public bool jumpCheck;
-        //public float jumpMotion;
-
-        //jump bool to actually jump
-        //public bool jumpBool;
-        //public float step_up_max = 1;   //prevents from manually moving above water
-        // public float selfGravity = 0.5f;       //used for jumping and naturally falling back into water, i.e. going through waterfalls
-        //private float waterFriction = 2;
-        //float placeHolder = 0;
-        //float varShootForward = 0;
-
         //Big Red Button Stop
         public bool bigRedButton;
 
@@ -50,11 +38,8 @@
         private void Start()
         {
             hullParent = GameObject.Find("ShipHull");
-            //playArea = GameObject.Find("SteamVRSDK");
             endMoth = GameObject.Find("roseMoth");
             endMoth.SetActive(false);
-
-            //circleNum = playArea.GetComponent<Transform>().rotation.y;
 
             endVideo = GameObject.Find("Ending (1)");
             endVideo.SetActive(false);
@@ -72,77 +57,11 @@
             dragonCount_2 = 0;
             mosqCount = 0;
             totalBugCount = 0;
-
-
-            //jumpMotion = 0;
-            //jumpCheck = false;
-            //jumpBool = false;
         }
-        /*
-        private void OnTriggerExit(Collider other)
-        {
-            ///RIVER COLLISION
-            if (other.gameObject.tag == "River")
-            {
-                jumpCheck = false;
-                //step_up_max = 1;
+      
 
-                //if moving through waterfall, then shoot forward and apply gravity
-                //if (hullParent.GetComponent<Collider>().bounds.min.y > other.GetComponent<Collider>().bounds.max.y)
-                //{
-                    //if (step_count_forward > 0 && jumpBool == false)  //moving forward and NOT jumping
-                    //{
-                    //print("Found the Bounds!!");
-                    //ShootForward();
-                    //}
-
-               // }
-
-            }
-
-        }
-        */
-        /*
-        private void OnTriggerStay(Collider other)
-        {
-            ///RIVER COLLISION
-            if (other.gameObject.tag == "River")
-            {
-                jumpCheck = true;   //we are colliding with a river, we can check if step_count_up == 3
-                                    //if this is true, then jump via 'jumpBool' changed in ButtonHeight script
-
-                //step_up_max is 0 when trying to go up, then 1 when trying to go down (multiplication for update funtion)
-                if (step_count_up > 0)
-                {
-                    //step_up_max = 0;
-                }
-                if (step_count_up <= 0)
-                    //step_up_max = 1;
-            }
-
-            //if moving through waterfall, then shoot forward and apply gravity
-            if (hullParent.GetComponent<Collider>().bounds.min.y > other.GetComponent<Collider>().bounds.min.y)
-            {
-                if (step_count_forward > 0 && jumpBool == false)  //moving forward and NOT jumping
-                {
-                    ShootForward();
-                }
-
-            }
-
-            //if(hullParent.GetComponent<Collider>().bounds.center > hullParent.GetComponent<Collider>().bounds.ClosestPoint()
-            //Debug.DrawLine(hullParent.GetComponent<Collider>().bounds.min, other.GetComponent<Collider>().bounds.max);
-            //print("Hull  --  x:  " + hullParent.GetComponent<Collider>().bounds.min.x + "  --  y: " + hullParent.GetComponent<Collider>().bounds.min.y + "  --  z: " + hullParent.GetComponent<Collider>().bounds.min.z);
-            //print("River --  x:  " + other.GetComponent<Collider>().bounds.min.x + "  --  y: " + other.GetComponent<Collider>().bounds.min.y + "  --  z: " + other.GetComponent<Collider>().bounds.min.z);
-
-        }
-        */
         private void OnTriggerEnter(Collider other)
         {
-            ///RIVER COLLISION
-            //if (jumpMotion < 0)
-            //    StopFall();
-            
 
             ///BUGS COLLISION
             if ( other.gameObject.tag == "Mosq")
@@ -188,6 +107,7 @@
                 if (waspCount == 4)
                     totalBugCount++;
             }
+
             /////////////////
             //ENDING BUG COLLISION
             if (other.gameObject.tag == "EndMoth")
@@ -198,15 +118,21 @@
             }
         }
 
+
+
         void ActivateMoth()
         {
                 endMoth.SetActive(true);
         }
 
+
+
         void StartEndVideo()
         {
             endVideo.SetActive(true);
         }
+
+
 
         void ZeroAllCounters()
         {
@@ -214,61 +140,8 @@
             step_count_right = 0;
             step_count_up = 0;
         }
-/*
-        // always running via moveDirection in Update()
-        //handles 'jumpMotion': is 0 when jumpBool == false, but iterates through a jump action if true
-        float JumpAction()
-        {
-            if(jumpBool == true)
-            {
-                jumpMotion -= selfGravity;
-                step_count_up = 0;
-            }
 
-            if (jumpBool == false)
-            {
-                if (jumpMotion < 0)
-                {
-                    jumpMotion += selfGravity;
-                }
 
-                if (jumpMotion >= 0)
-                    jumpMotion = 0;
-            }
-            return jumpMotion;
-        }
-
-        void ShootForward()
-        {
-            jumpBool = true;
-            placeHolder = step_count_forward;
-            step_count_forward = 0;
-            jumpMotion = 10;
-            Invoke("StopShoot", 1);
-        }
-
-        void StopShoot()
-        {
-            step_count_forward = placeHolder;
-        }
-        
-        void StopFall()
-        {
-            jumpBool = false;
-            jumpMotion = -17f;
-        }
-        */
-
-        //void TurnPlayArea()
-        //{
-        //    print("circleNum - BEFORE: " + circleNum);
-//
-         //   circleNum += 90;
-         //   if (circleNum >= 360)
-         //       circleNum = 0;
-
-         //   playArea.GetComponent<Transform>().Rotate(new Vector3(0, circleNum, 0));
-        //}
 
         private void Update()
         {
@@ -278,27 +151,20 @@
             if (totalBugCount == 4)
                 ActivateMoth();
 
-           // if (Input.GetButtonDown("Right"))
-             //   TurnPlayArea();
-
             CharacterController controller = GetComponent<CharacterController>();
             
             //forward is negative b/c fish is set up wrong   
             moveDirection = new Vector3(-step_count_forward*1.5f, (step_count_up * 1.5f), 0f);
 
-            //right*3 b/c it makes it faster
+            //right*10 b/c it makes it faster
             hullParent.transform.Rotate(new Vector3(0f, step_count_right * 10 * Time.deltaTime, 0f));
 
             moveDirection = transform.TransformDirection(moveDirection);
             moveDirection *= speed;
 
             controller.Move(moveDirection * Time.deltaTime);
-
-            //Debug.Log("jumpCheck: " + jumpCheck);
-            //Debug.Log("jumpBool: " + jumpBool);
-
-        }
-
-        
+        }   
     }
+
+
 }
